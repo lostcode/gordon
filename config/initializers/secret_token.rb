63b2fba4d66b6f7e147a51a010902632988b5710
Manoj Ganesan <manojganesan@gamestop.com>
2013-07-16 22:05:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gordon::Application.config.secret_token = 'eaf4368b5e2bd4e2950abe319e0287dfd0046b7d44d9c54d549035859b37901bc440b130777636f295457e689c2c0dfe7ff79bb27d9125600cb4117c3adc15a5'
